b304977d8c20bac071aef66af6a12d37f19514ac
乔星星 <joannaready123@gmail.com>
2015-03-20 20:27:11 +0800

    init 项目框架初步搭建

---
+++ miniShop/miniShop/source/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  miniShop
+//
+//  Created by qiao xingxing on 15/3/20.
+//  Copyright (c) 2015年 qiao xingxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ miniShop/miniShop/source/addNew/addProductController.h
@@ -0,0 +1,13 @@
+//
+//  addProductController.h
+//  miniShop
+//
+//  Created by qiao xingxing on 15/3/20.
+//  Copyright (c) 2015年 qiao xingxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface addProductController : UIViewController
+
+@end
