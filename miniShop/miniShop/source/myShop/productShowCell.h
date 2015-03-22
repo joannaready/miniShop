1766a2e53fa01059752dbaf344e8d0549c42f6ca
乔星星 <joannaready123@gmail.com>
2015-03-23 03:23:11 +0800

    我的微店和表 cell 自定义 XIB

---
+++ miniShop/miniShop/source/myShop/productShowCell.h
@@ -0,0 +1,13 @@
+//
+//  productShowCell.h
+//  miniShop
+//
+//  Created by qiao xingxing on 15/3/23.
+//  Copyright (c) 2015年 qiao xingxing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface productShowCell : UITableViewCell
+
+@end
---
+++ miniShop/miniShop/source/myShop/productShowCell.m
@@ -0,0 +1,23 @@
+//
+//  productShowCell.m
+//  miniShop
+//
+//  Created by qiao xingxing on 15/3/23.
+//  Copyright (c) 2015年 qiao xingxing. All rights reserved.
+//
+
+#import "productShowCell.h"
+
+@implementation productShowCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
